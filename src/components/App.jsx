4c558e43997e2e2a1fda43f9b8a124ecf64a99a6
sharukhsm <sharukhsm@gmail.com>
2021-09-01 15:59:18 +0530

    Complete ToDoList app with delete function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,17 @@ function App() {
     setInputText("");
   }
 
+  //2.0 Passing deleteItem function as props
+  //2.6 Getting id from onChecked
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+        //Even if you don't use the item parameter, we should include it in the filter function.
+      });
+    });
+  }
+
   return (
     <div className="container">
       <div className="heading">
@@ -30,8 +41,19 @@ function App() {
       </div>
       <div>
         <ul>
-          {items.map((todoItem) => (
-            <ToDoItem item={todoItem} />
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              //2.3 Add key and id to uniquely identify each note item while deletion or any modification
+              key={index}
+              //We should be passing it as an id
+              id={index}
+              text={todoItem}
+              // 2.1 Passing deleteItem function as props
+              //2.5 Getting id and sending it to delete function
+              onChecked={deleteItem}
+
+              //React recommends not to use index for the key and instead suggests to use UUID.
+            />
           ))}
         </ul>
       </div>
@@ -40,3 +62,45 @@ function App() {
 }
 
 export default App;
+
+// 1.0. Strike through method
+// function App() {
+//   const [inputText, setInputText] = useState("");
+//   const [items, setItems] = useState([]);
+
+//   function handleChange(event) {
+//     const newValue = event.target.value;
+//     setInputText(newValue);
+//   }
+
+//   function addItem() {
+//     setItems((prevItems) => {
+//       return [...prevItems, inputText];
+//     });
+//     setInputText("");
+//   }
+
+//   return (
+//     <div className="container">
+//       <div className="heading">
+//         <h1>To-Do List</h1>
+//       </div>
+//       <div className="form">
+//         <input onChange={handleChange} type="text" value={inputText} />
+//         <button onClick={addItem}>
+//           <span>Add</span>
+//         </button>
+//       </div>
+//       <div>
+//         <ul>
+//           {items.map((todoItem) => (
+//             <ToDoItem
+//               item={todoItem} />
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,21 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
 
 function ToDoItem(props) {
-  const [isDone, setIsDone] = useState(false);
-
-  function handleClick() {
-    setIsDone((prevValue) => {
-      return !prevValue;
-    });
-  }
-
   return (
-    <div onClick={handleClick}>
-      <li style={{ textDecoration: isDone ? "line-through" : null }}>
-        {props.item}
-      </li>
+    // 2.2 Getting onChecked from props
+    //2.4 Sending id back to onChecked function
+    /*2.7  <div onClick={props.onChecked(props.id)}>. In JS whenever
+     there is something inside a function, we are calling it rather than passing it. In the above case
+     props.id can't be passed like this. If we do the ToDoItem components renders 
+     every time we call it and that is not what we want. We just want the id only when the ToDoItem is clicked. 
+     So instead we can create a function inside the onClick so, when the ToDoItem is clicked only then
+      the function is triggered. */
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
     </div>
   );
 }
 
 export default ToDoItem;
+
+// 1.0. Strike-through method
+// function ToDoItem(props) {
+//   const [isDone, setIsDone] = useState(false);
+
+//   function handleClick() {
+//     setIsDone((prevValue) => {
+//       return !prevValue;
+//     });
+//   }
+
+//   return (
+//     <div onClick={handleClick}>
+//       <li style={{ textDecoration: isDone ? "line-through" : null }}>
+//         {props.item}
+//       </li>
+//     </div>
+//   );
+// }
+
+// export default ToDoItem;
